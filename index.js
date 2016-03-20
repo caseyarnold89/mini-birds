5c767030a736f0372ef6e095e9b8075fe50e12d3
Casey Arnold <caseyarnold89@gmail.com>
2016-03-20 16:22:23 -0600

    Steps 1-2 complete

--- index.js
+++ index.js
@@ -6,8 +6,28 @@ var mongojs = require('mongojs');
 var app = express();
 var port = 9050;
 
+var db = mongojs('birds', ['products']);
+
 app.use(bodyParser.json());
 
+app.post('/api/sighting', function(req, res) {
+    
+    
+    
+    return res.status(200).json("Post works");
+});
+app.get('/api/sighting', function(req, res) {
+    console.log(req.query);
+    return res.status(200).json("Get works");
+});
+app.put('/api/sighting', function(req, res) {
+    console.log(req.query);
+    return res.status(200).json("Put works");
+});
+app.delete('/api/sighting',function(req, res) {
+    console.log(req.query);
+    return res.status(200).json("Delete works");
+});
 app.listen(port, function(err) {
     if (err) {
         return console.error(err);
