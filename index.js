3a059635dea7c404c3b04033bea0f70ef57cba26
Casey Arnold <caseyarnold89@gmail.com>
2016-03-20 20:19:06 -0600

    Project Finished

--- index.js
+++ index.js
@@ -6,27 +6,62 @@ var mongojs = require('mongojs');
 var app = express();
 var port = 9050;
 
-var db = mongojs('birds', ['products']);
+var db = mongojs('birds', ['sightings']);
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/api/sighting', function(req, res) {
     
+    db.sightings.save(req.body, function (err, response) {
+        if (err) return res.status(500).json(err)
+        else return res.json(response);
+
+    })
     
-    
-    return res.status(200).json("Post works");
 });
+
 app.get('/api/sighting', function(req, res) {
-    console.log(req.query);
-    return res.status(200).json("Get works");
+    var query = {};
+    
+    if (req.query.region) { 
+        query = { region: req.query.region} 
+    }
+    else if (req.query.species) {
+        query = { species: req.query.species }
+    } 
+    db.sightings.find(query, function(err, response){
+        if(err) return res.stats(500).json(err)
+        else return res.json(response);
+    })
 });
 app.put('/api/sighting', function(req, res) {
-    console.log(req.query);
-    return res.status(200).json("Put works");
+    
+    if (!req.query.id) return res.status(418).send('request query id required');
+    db.sightings.findAndModify(
+        { query: {
+            _id: mongojs.ObjectId(req.query.id)
+            },
+          update: {
+              $set: req.body
+          }
+        },
+        function(err, response) {
+            if(err) return res.status(500).json(err)
+            else return res.json(response)
+        }
+    )
+
 });
 app.delete('/api/sighting',function(req, res) {
-    console.log(req.query);
-    return res.status(200).json("Delete works");
+    if(!req.query.id) return res.status(418).send('request query id required');
+    db.sightings.remove(
+        { _id: mongojs.ObjectId(req.query.id) }, 
+        function (err, response) {
+            if(err) return res.status(500).json(err)
+            else return res.json(response);
+        }
+    )
 });
 app.listen(port, function(err) {
     if (err) {
